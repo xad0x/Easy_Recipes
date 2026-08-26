281ea0b2661e884fc9b235e71a4e6a166fc6c3fc
xad0x <152625618+xad0x@users.noreply.github.com>
2026-08-26 20:18:36 +0200

    Initial commit

---
+++ src/client/java/org/xad0x/easy_recipes/client/CustomrecipeClient.java
@@ -0,0 +1,10 @@
+package org.xad0x.easy_recipes.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CustomrecipeClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/xad0x/easy_recipes/easy_recipes.java
@@ -0,0 +1,10 @@
+package org.xad0x.easy_recipes;
+
+import net.fabricmc.api.ModInitializer;
+
+public class easy_recipes implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
